893be174c0d707e3f54abec5a9a0bdb7d338a865
Gabrielv0id <gabriel1998.fonseca@gmail.com>
2023-05-16 16:54:26 -0300

    fix lint error 2.0

--- app/backend/src/database/models/MatchModel.ts
+++ app/backend/src/database/models/MatchModel.ts
@@ -1,9 +1,15 @@
-import { Model, InferAttributes, InferCreationAttributes, CreationOptional, DataTypes } from 'sequelize';
+import { 
+  Model,
+  InferAttributes,
+  InferCreationAttributes,
+  CreationOptional,
+  DataTypes 
+} from 'sequelize';
 import db from '.';
 import Team from './TeamModel';
 
-export default class Match 
-extends Model<InferAttributes<Match>, InferCreationAttributes<Match>> {
+export default class Match
+  extends Model<InferAttributes<Match>, InferCreationAttributes<Match>> {
   declare id: CreationOptional<number>;
   declare homeTeamId: number;
   declare homeTeamGoals: number;
@@ -13,52 +19,51 @@ extends Model<InferAttributes<Match>, InferCreationAttributes<Match>> {
 }
 
 Match.init({
-    id: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
-      primaryKey: true,
-      allowNull: false,
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+    allowNull: false,
+  },
+  homeTeamId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: 'teams',
+      key: 'id',
     },
-    homeTeamId: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      references: {
-        model: 'teams',
-        key: 'id',
-      },
-      field: 'home_team_id',
+    field: 'home_team_id',
+  },
+  homeTeamGoals: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    field: 'home_team_goals',
+  },
+  awayTeamId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: 'teams',
+      key: 'id',
     },
-    homeTeamGoals: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      field: 'home_team_goals',
-    },
-    awayTeamId: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      references: {
-        model: 'teams',
-        key: 'id',
-      },
-      field: 'away_team_id',
-    },
-    awayTeamGoals: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      field: 'away_team_goals',
-    },
-    inProgress: {
-      type: DataTypes.BOOLEAN,
-      allowNull: false,
-      field: 'in_progress',
-    },
-  }, {
-    modelName: 'matches',
-    sequelize: db,
-    underscored: true,
-    timestamps: false,
-  }
-);
+    field: 'away_team_id',
+  },
+  awayTeamGoals: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    field: 'away_team_goals',
+  },
+  inProgress: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+    field: 'in_progress',
+  },
+}, {
+  modelName: 'matches',
+  sequelize: db,
+  underscored: true,
+  timestamps: false,
+});
 
 Team.hasMany(Match, { foreignKey: 'homeTeamId', as: 'homeTeam' });
 Team.hasMany(Match, { foreignKey: 'awayTeamId', as: 'awayTeam' });
--- app/backend/src/database/models/UserModel.ts
+++ app/backend/src/database/models/UserModel.ts
@@ -1,8 +1,14 @@
-import { Model, InferAttributes, InferCreationAttributes, CreationOptional, DataTypes } from 'sequelize';
+import { 
+  Model,
+  InferAttributes,
+  InferCreationAttributes,
+  CreationOptional,
+  DataTypes 
+} from 'sequelize';
 import db from '.';
 
 export default class User 
-extends Model< InferAttributes<User>, InferCreationAttributes<User>> {
+  extends Model< InferAttributes<User>, InferCreationAttributes<User>> {
   declare id: CreationOptional<number>;
   declare username: string;
   declare role: string;
@@ -11,32 +17,31 @@ extends Model< InferAttributes<User>, InferCreationAttributes<User>> {
 }
 
 User.init({
-    id: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
-      primaryKey: true,
-      allowNull: false,
-    },
-    username: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    role: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-  }, {
-    modelName: 'users',
-    sequelize: db,
-    underscored: true,
-    timestamps: false,
-  }
-);
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+    allowNull: false,
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  role: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+}, {
+  modelName: 'users',
+  sequelize: db,
+  underscored: true,
+  timestamps: false,
+});
