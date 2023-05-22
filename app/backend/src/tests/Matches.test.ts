e90c5acf761ee3674e8db9458e4303a4489b94c6
Gabrielv0id <gabriel1998.fonseca@gmail.com>
2023-05-22 17:04:14 -0300

    implements tests for inprogress searchs

--- app/backend/src/tests/Matches.test.ts
+++ app/backend/src/tests/Matches.test.ts
@@ -27,5 +27,21 @@ describe('Matches', () => {
       expect(chaiHttpResponse.status).to.be.equal(200);
       expect(chaiHttpResponse.body).to.deep.equal(MockedMatches);
     });
+
+    it('caso o inprogress esteja marcado true retorna status 200 e as partidas com true', async () => {
+      sinon.stub(Match, 'findAll').resolves([MockedMatches[1], MockedMatches[3]] as unknown as Match[]);
+      chaiHttpResponse = await chai.request(app).get('/matches?inProgress=true');
+
+      expect(chaiHttpResponse.status).to.be.equal(200);
+      expect(chaiHttpResponse.body).to.be.deep.equal([MockedMatches[1], MockedMatches[3]]);
+    });
+
+    it('caso o inprogress esteja marcado false retorna status 200 e as partidas com false', async () => {
+      sinon.stub(Match, 'findAll').resolves([MockedMatches[0], MockedMatches[2]] as unknown as Match[]);
+      chaiHttpResponse = await chai.request(app).get('/matches?inProgress=false');
+
+      expect(chaiHttpResponse.status).to.be.equal(200);
+      expect(chaiHttpResponse.body).to.be.deep.equal([MockedMatches[0], MockedMatches[2]]);
+    });
   });
 });
